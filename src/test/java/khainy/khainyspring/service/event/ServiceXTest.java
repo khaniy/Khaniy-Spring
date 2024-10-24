f4d980ea240ff8c9cccc471b77df54bdbe014627
khaniy <kangh1596@gmail.com>
2024-10-24 18:00:12 +0900

    [Feat] - Spring Event Test 코드 수정

--- src/test/java/khainy/khainyspring/service/event/ServiceXTest.java
+++ src/test/java/khainy/khainyspring/service/event/ServiceXTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.RecordApplicationEvents;
 
 @SpringBootTest
+@RecordApplicationEvents
 class ServiceXTest {
 
     @Autowired
